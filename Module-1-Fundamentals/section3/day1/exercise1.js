52ef75d226296e48ff7859bdd45adef817d947e4
eduardo pereira filho <eduardopereirafilho6@gmail.com>
2023-03-29 14:22:59 -0300

    7th exercise

--- Module-1-Fundamentals/section3/day1/exercise1.js
+++ Module-1-Fundamentals/section3/day1/exercise1.js
@@ -1,10 +1,9 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27], smaller = numbers[0];
+let numbers = [];
 
-for (let i = 0; i < numbers.length; i++){
-    if(numbers[i] < smaller){
-       smaller = numbers[i];
-    }
+for (let i = 1; i <= 25; i++){
+    numbers.push(i);
 } 
 
-console.log(smaller);
-
+for(let j = 0; j < numbers.length; j++){
+    console.log(numbers[j] / 2);
+}
