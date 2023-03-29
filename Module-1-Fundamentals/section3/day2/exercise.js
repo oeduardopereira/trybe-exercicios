88adda8ef71166948db5d1d1b2c52a3a7cd284f4
eduardo pereira filho <eduardopereirafilho6@gmail.com>
2023-03-29 19:43:18 -0300

    6th exercise

--- Module-1-Fundamentals/section3/day2/exercise.js
+++ Module-1-Fundamentals/section3/day2/exercise.js
@@ -1,10 +1,19 @@
-let n = 5;
-let s = '*';
+let n = 10;
+let whiteSpace = " ";
+let ast = "*";
+let result = "";
+let x = n;
 
-for(let i = 0; i < n; i++){
-    //for(let j = 0; j < n; j++){
-        process.stdout.write(s);
-        s += "*";
-   // }
-    console.log();
+for (let i = 0; i < n ; i++) {
+    for(let j = 0; j < n; j++) {
+        if(j < x - 1){
+            result += whiteSpace;
+        }
+        else{
+            result += ast;
+        }
+    }
+    x--;
+    console.log(result);
+    result = "";
 }
