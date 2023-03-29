407c5775c1804f1ade7dd2ca193d57c180bf9bdf
eduardo pereira filho <eduardopereirafilho6@gmail.com>
2023-03-29 14:16:12 -0300

    4th exercise

--- Module-1-Fundamentals/section3/day1/exercise1.js
+++ Module-1-Fundamentals/section3/day1/exercise1.js
@@ -3,4 +3,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27], contador = 0;
 for (let i = 0; i < numbers.length; i++){
     contador += numbers[i];
 } 
-console.log(contador / numbers.length);
+
+let avg = contador / numbers.length;
+if(avg > 20) console.log("Maior que 20");
+else console.log("Menor ou igual a 20"); 
