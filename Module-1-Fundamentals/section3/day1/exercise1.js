9a960914d2f3e5ce42d8293ebc151de6c1157d0b
eduardo pereira filho <eduardopereirafilho6@gmail.com>
2023-03-29 14:13:00 -0300

    3rd exercise

--- Module-1-Fundamentals/section3/day1/exercise1.js
+++ Module-1-Fundamentals/section3/day1/exercise1.js
@@ -3,4 +3,4 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27], contador = 0;
 for (let i = 0; i < numbers.length; i++){
     contador += numbers[i];
 } 
-console.log(contador);
+console.log(contador / numbers.length);
