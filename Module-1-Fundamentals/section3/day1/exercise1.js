57d7f2f3f50d4112d034e5c7f1fba8075dcd76b3
eduardo pereira filho <eduardopereirafilho6@gmail.com>
2023-03-29 14:20:25 -0300

    6th exercise

--- Module-1-Fundamentals/section3/day1/exercise1.js
+++ Module-1-Fundamentals/section3/day1/exercise1.js
@@ -1,11 +1,10 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27], contador = 0;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27], smaller = numbers[0];
 
 for (let i = 0; i < numbers.length; i++){
-    if(numbers[i] % 2 != 0){
-        console.log(numbers[i]);
-        contador++;
+    if(numbers[i] < smaller){
+       smaller = numbers[i];
     }
 } 
-if(contador === 0){
-    console.log("Nao existe numero impar!");
-    }
+
+console.log(smaller);
+
