45c32811619f776ac96243d0173ad400442b48b3
eduardo pereira filho <eduardopereirafilho6@gmail.com>
2023-03-29 15:34:45 -0300

    4th exercise

--- Module-1-Fundamentals/section3/day2/exercise.js
+++ Module-1-Fundamentals/section3/day2/exercise.js
@@ -1,13 +1,8 @@
-// resultado esperado:
-let n =[5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let arr = [];
+let n = 5;
 
-for (let i = 0; i < n.length; i++){
-    if(i === n.length - 1){
-        arr.push(n[i] * 2);
-    }else{
-        arr.push(n[i] * n[i + 1]);
+for(let i = 0; i < n; i++){
+    for(let j = 0; j < n; j++){
+        process.stdout.write("*");
     }
+    console.log();
 }
-
-console.log(arr);
