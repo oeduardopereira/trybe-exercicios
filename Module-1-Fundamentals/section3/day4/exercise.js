44b38c667f0079f8f87f4ab3ab863a83dfe17239
eduardo pereira filho <eduardopereirafilho6@gmail.com>
2023-04-03 18:48:47 -0300

    Exercise 1

--- Module-1-Fundamentals/section3/day4/exercise.js
+++ Module-1-Fundamentals/section3/day4/exercise.js
@@ -1,31 +1,37 @@
-let student1 = {
-    html: 'Muito Bom',
-    css: 'Bom',
-    javascript: 'Ótimo',
-    softskills: 'Ótimo',
-  };
-  
-  let student2 = {
-    html: 'Bom',
-    css: 'Ótimo',
-    javascript: 'Ruim',
-    softskills: 'Sexo',
-  };
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
 
-  function print(obj) {
-    for (let key in obj) {
-        console.log(key + ': ' + obj[key]);
-    }
-  };
-let std3 = [];
-  print(student1);
-  console.log("");
-  console.log(Object.keys(student2));
-  console.log(Object.values(student2));
-  console.log();
+console.log('Bem-vindo, ' + info.personagem);
 
-  console.log(Object.entries(student1));
-  
-  console.log();
-  Object.assign(std3, student2);
-  console.log(std3);
+info['recorrente'] = 'Sim';
+
+console.log();
+
+for (let key in info) {
+  console.log(key);
+}
+
+console.log();
+
+for (let value in info) {
+  console.log(info[value]);
+}
+
+console.log();
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+}
+
+for (let value in info) {
+  if (info[value] === 'Sim' && info2[value] === 'Sim'){
+    console.log('Ambos recorrentes');
+  }
+  console.log(info[value] + ' e ' + info2[value]);
+}
