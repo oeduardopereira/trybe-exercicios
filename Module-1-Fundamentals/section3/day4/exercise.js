f92c652c914528cf68cc50a175f66010b6e0b99f
eduardo pereira filho <eduardopereirafilho6@gmail.com>
2023-04-03 19:20:22 -0300

    Exercise 3 - costumerInfo

--- Module-1-Fundamentals/section3/day4/exercise.js
+++ Module-1-Fundamentals/section3/day4/exercise.js
@@ -1,25 +1,49 @@
-let leitor = {
-  nome: 'Julia',
-  sobrenome: 'Pessoa',
-  idade: 21,
-  livrosFavoritos: [
-    {
-      titulo: 'O Pior Dia de Todos',
-      autor: 'Daniela Kopsch',
-      editora: 'Tordesilhas',
+let order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      },
     },
-  ],
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      },
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
 };
 
- console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome +
-  ' se chama "' + leitor.livrosFavoritos[0].titulo + '".');
+function customerInfo(order) {
+  console.log('Ola, ' + order.order.delivery.deliveryPerson + ', entrega para: ' + 
+  order.name + ', Telefone: ' + order.phoneNumber + ', R. ' + order.address.street + 
+  ', N: ' + order.address.number + ', AP: ' + order.address.apartment);
+}
 
-  console.log();
- 
-  leitor.livrosFavoritos[1] = {
-    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-    autor: 'JK Rowling',
-    editora: 'Rocco',
-  }
+customerInfo(order);
 
-console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos');
+function orderModifier(order) {
+  // Adicione abaixo as informações necessárias.
+}
+
+orderModifier(order);
