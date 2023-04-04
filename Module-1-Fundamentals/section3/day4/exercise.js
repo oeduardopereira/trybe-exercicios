3b0cd7e6903a38d7caf96616d708770f2db5db4e
eduardo pereira filho <eduardopereirafilho6@gmail.com>
2023-04-04 14:17:16 -0300

    exercise bonus 1

--- Module-1-Fundamentals/section3/day4/exercise.js
+++ Module-1-Fundamentals/section3/day4/exercise.js
@@ -1,59 +1,29 @@
-let order = {
-  name: 'Rafael Andrade',
-  phoneNumber: '11-98763-1416',
-  address: {
-    street: 'Rua das Flores',
-    number: '389',
-    apartment: '701',
-  },
-  order: {
-    pizza: {
-      marguerita: {
-        amount: 1,
-        price: 25,
-      },
-      pepperoni: {
-        amount: 1,
-        price: 20,
-      },
-    },
-    drinks: {
-      coke: {
-        type: 'Coca-Cola Zero',
-        price: 10,
-        amount: 1,
-      },
-    },
-    delivery: {
-      deliveryPerson: 'Ana Silveira',
-      price: 5,
-    },
-  },
-  payment: {
-    total: 60,
-  },
+let lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
 };
 
-function customerInfo(order) {
-  console.log('Ola, ' + order.order.delivery.deliveryPerson + ', entrega para: ' + 
-  order.name + ', Telefone: ' + order.phoneNumber + ', R. ' + order.address.street + 
-  ', N: ' + order.address.number + ', AP: ' + order.address.apartment);
+let lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+let lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+function addTurnLesson2(obj, key, value) {
+  obj[key] = value;
+  console.log(obj);
 }
 
-customerInfo(order);
+let turno = 'turno';
+let noite = 'noite';
 
-console.log();
-
-order.name = 'Luiz Silva';
-order.payment.total = 50;
-
-function orderModifier(order) {
-  let pizzas = Object.keys(order.order.pizza);
-
-
-  console.log('Ola, ' + order.name + ', o valor total de seu pedido de ' + 
-  pizzas[0] + ', ' + pizzas[1] + ' e ' + order.order.drinks.coke.type +
-  ' e R$' + order.payment.total);
-}
-
-orderModifier(order);
+addTurnLesson2(lesson2, turno, noite);
