961797af2a255da648a13c9cd52b96ed517a2f43
eduardo pereira filho <eduardopereirafilho6@gmail.com>
2023-04-11 15:57:29 -0300

    1st exercise

---
+++ Module-1-Fundamentals/section4/day2/exercise.js
@@ -0,0 +1,8 @@
+const way = document.getElementById('where-are-you').parentElement;
+        way.style.backgroundColor = 'red';
+
+const firstChildOfChild = document.getElementById('first-child-of-child');
+firstChildOfChild.innerText = 'adjhdaksdjbdjkabd dkjabdakdbdk  adbadjabsd adbdkdbbjlas';
+
+const parentOfFirstChild = document.getElementById('parent').firstElementChild;
+parentOfFirstChild.innerText = 'First child by parent';
