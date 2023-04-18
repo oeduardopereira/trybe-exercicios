5adc05e699862dd25a93e37a9a02387de30edfc0
eduardo pereira filho <eduardopereirafilho6@gmail.com>
2023-04-18 14:55:29 -0300

    set font size

--- Module-1-Fundamentals/section4/day4/script.js
+++ Module-1-Fundamentals/section4/day4/script.js
@@ -12,6 +12,12 @@ window.onload = () => {
         localStorage.setItem('fontColor', color);
     };
 
+    const setFontSize = (size) => {
+        const fontSize = document.querySelector('.content');
+        fontSize.style.fontSize = size;
+        localStorage.setItem('fontSize', size);
+    };
+
     // return the color of the button selected
     const backgroundColorBTN = document.querySelectorAll('#background-color>button');
     for (let i = 0; i < backgroundColorBTN.length; i += 1) {
@@ -27,12 +33,22 @@ window.onload = () => {
             setTextColor(event.target.innerHTML);
         })
     }
+    
+    // return the size of the text to be changed
+    const getFontSize = document.querySelectorAll('#font-size>button');
+    for (let i = 0; i < getFontSize.length; i += 1) {
+        getFontSize[i].addEventListener('click', (event) => {
+            setFontSize(event.target.innerHTML);
+        })
+    }
 
     const initialize = () => {
         let backgroundColor = localStorage.getItem("backgroundColor")
         if (backgroundColor) setBackgroundColor(backgroundColor);
         let fontColor = localStorage.getItem('fontColor');
         if (fontColor) setTextColor(fontColor);
+        let fontSize = localStorage.getItem('fontSize');
+        if (fontSize) setFontSize(fontSize);
     }
     initialize();
 }
