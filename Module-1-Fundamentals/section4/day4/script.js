0e327b988bd62e7c6f00ce68ae7ec886c842d70f
eduardo pereira filho <eduardopereirafilho6@gmail.com>
2023-04-18 15:08:23 -0300

    set font family and line height

--- Module-1-Fundamentals/section4/day4/script.js
+++ Module-1-Fundamentals/section4/day4/script.js
@@ -18,6 +18,18 @@ window.onload = () => {
         localStorage.setItem('fontSize', size);
     };
 
+    const setLinePadding = (padding) => {
+            const linePadding = document.querySelector('.content');
+            linePadding.style.lineHeight = padding;
+            localStorage.setItem('linePadding', padding);
+    };
+    
+    const setFontStyle = (style) => {
+            const fontStyle = document.querySelector('.content');
+            fontStyle.style.fontFamily = style;
+            localStorage.setItem('fontFamily', style);
+    };
+
     // return the color of the button selected
     const backgroundColorBTN = document.querySelectorAll('#background-color>button');
     for (let i = 0; i < backgroundColorBTN.length; i += 1) {
@@ -41,14 +53,38 @@ window.onload = () => {
             setFontSize(event.target.innerHTML);
         })
     }
+    
+    // return the line padding of the text to be changed
+    const getLinePadding = document.querySelectorAll('#line-height>button');
+    for (let i = 0; i < getLinePadding.length; i += 1) {
+        getLinePadding[i].addEventListener('click', (event) => {
+            setLinePadding(event.target.innerHTML);
+        })
+    }
+    
+    // return the font style of the text to be changed
+    const getFontStyle = document.querySelectorAll('#font-family>button');
+    for (let i = 0; i < getFontStyle.length; i += 1) {
+        getFontStyle[i].addEventListener('click', (event) => {
+            setFontStyle(event.target.innerHTML);
+        })
+    }
 
     const initialize = () => {
         let backgroundColor = localStorage.getItem("backgroundColor")
         if (backgroundColor) setBackgroundColor(backgroundColor);
+
         let fontColor = localStorage.getItem('fontColor');
         if (fontColor) setTextColor(fontColor);
+
         let fontSize = localStorage.getItem('fontSize');
         if (fontSize) setFontSize(fontSize);
+
+        let linePadding = localStorage.getItem('linePadding');
+        if (linePadding) setLinePadding(linePadding);
+        
+        let fontStyle = localStorage.getItem('fontStyle');
+        if (fontStyle) setFontStyle(fontStyle);
     }
     initialize();
 }
