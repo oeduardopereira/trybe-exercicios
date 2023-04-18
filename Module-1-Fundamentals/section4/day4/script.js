e72ba49feae57073a2d6ccbdc167bd45eae4a6f3
eduardo pereira filho <eduardopereirafilho6@gmail.com>
2023-04-18 14:51:21 -0300

    set font color

--- Module-1-Fundamentals/section4/day4/script.js
+++ Module-1-Fundamentals/section4/day4/script.js
@@ -5,6 +5,12 @@ window.onload = () => {
         paragram.style.backgroundColor = color;
         localStorage.setItem('backgroundColor', color);
     };
+    
+    const setTextColor = (color) => {
+        const fontColor = document.querySelector('.content');
+        fontColor.style.color = color;
+        localStorage.setItem('fontColor', color);
+    };
 
     // return the color of the button selected
     const backgroundColorBTN = document.querySelectorAll('#background-color>button');
@@ -14,9 +20,19 @@ window.onload = () => {
     });
     }
 
+    // return the color of the text to be changed
+    const getColorText = document.querySelectorAll('#font-color>button');
+    for (let i = 0; i < getColorText.length; i += 1) {
+        getColorText[i].addEventListener('click', (event) => {
+            setTextColor(event.target.innerHTML);
+        })
+    }
+
     const initialize = () => {
         let backgroundColor = localStorage.getItem("backgroundColor")
         if (backgroundColor) setBackgroundColor(backgroundColor);
+        let fontColor = localStorage.getItem('fontColor');
+        if (fontColor) setTextColor(fontColor);
     }
     initialize();
 }
