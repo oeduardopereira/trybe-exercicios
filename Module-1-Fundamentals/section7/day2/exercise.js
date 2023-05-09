fa8a57f861c941d6d5f3ade9d61ce66a1a677c34
eduardo pereira filho <eduardopereirafilho6@gmail.com>
2023-05-09 19:30:49 -0300

    umamaquina

--- Module-1-Fundamentals/section7/day2/exercise.js
+++ Module-1-Fundamentals/section7/day2/exercise.js
@@ -1,90 +1,112 @@
-const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
-let n = numbers.filter((number) => {return number % 2 === 0});
-let x = n.reduce((acc, curr) => {
-    return acc + curr;
-}, 0);
-console.log(x);
+// Fonte: <https://restcountries.com/v2/all>
+const countries = [
+  {
+    name: 'Afghanistan',
+    region: 'Asia',
+    currencies: [{ code: 'AFN', name: 'Afghan afghani' }],
+    capital: 'Kabul',
+    population: 40218234,
+    area: 652230
+  },
+  {
+    name: 'Åland Islands',
+    region: 'Europe',
+    currencies: [{ code: 'EUR', name: 'Euro' }],
+    capital: 'Mariehamn',
+    population: 28875,
+    area: 1580
+  },
+  {
+    name: 'Albania',
+    region: 'Europe',
+    currencies: [{ code: 'ALL', name: 'Albanian lek' }],
+    capital: 'Tirana',
+    population: 2837743,
+    area: 28748
+  },
+  {
+    name: 'Algeria',
+    region: 'Africa',
+    currencies: [{ code: 'DZD', name: 'Algerian dinar' }],
+    capital: 'Algiers',
+    population: 44700000,
+    area: 2381741
+  },
+  {
+    name: 'American Samoa',
+    region: 'Oceania',
+    currencies: [{ code: 'USD', name: 'United States Dollar' }],
+    capital: 'Pago Pago',
+    population: 55197,
+    area: 199
+  },
+  {
+    name: 'Andorra',
+    region: 'Europe',
+    currencies: [{ code: 'EUR', name: 'Euro' }],
+    capital: 'Andorra la Vella',
+    population: 77265,
+    area: 468
+  },
+  {
+    name: 'Angola',
+    region: 'Africa',
+    currencies: [{ code: 'AOA', name: 'Angolan kwanza' }],
+    capital: 'Luanda',
+    population: 32866268,
+    area: 1246700
+  },
+  {
+    name: 'Anguilla',
+    region: 'Americas',
+    currencies: [{ code: 'XCD', name: 'East Caribbean dollar' }],
+    capital: 'The Valley',
+    population: 13452,
+    area: 91
+  }
+];
 
-const estudantes = [
-    {
-      nome: 'Jorge',
-      sobrenome: 'Silva',
-      idade: 14,
-      turno: 'Manhã',
-      materias: [
-        { name: 'Matemática', nota: 67 },
-        { name: 'Português', nota: 79 },
-        { name: 'Química', nota: 70 },
-        { name: 'Biologia', nota: 65 },
-      ],
-    },
-    {
-      nome: 'Mario',
-      sobrenome: 'Ferreira',
-      idade: 15,
-      turno: 'Tarde',
-      materias: [
-        { name: 'Matemática', nota: 59 },
-        { name: 'Português', nota: 80 },
-        { name: 'Química', nota: 78 },
-        { name: 'Biologia', nota: 92 },
-      ],
-    },
-    {
-      nome: 'Jorge',
-      sobrenome: 'Santos',
-      idade: 15,
-      turno: 'Manhã',
-      materias: [
-        { name: 'Matemática', nota: 76 },
-        { name: 'Português', nota: 90 },
-        { name: 'Química', nota: 70 },
-        { name: 'Biologia', nota: 80 },
-      ],
-    },
-    {
-      nome: 'Maria',
-      sobrenome: 'Silveira',
-      idade: 14,
-      turno: 'Manhã',
-      materias: [
-        { name: 'Matemática', nota: 91 },
-        { name: 'Português', nota: 85 },
-        { name: 'Química', nota: 92 },
-        { name: 'Biologia', nota: 90 },
-      ],
-    },
-    {
-      nome: 'Natalia',
-      sobrenome: 'Castro',
-      idade: 14,
-      turno: 'Manhã',
-      materias: [
-        { name: 'Matemática', nota: 70 },
-        { name: 'Português', nota: 70 },
-        { name: 'Química', nota: 60 },
-        { name: 'Biologia', nota: 50 },
-      ],
-    },
-    {
-      nome: 'Wilson',
-      sobrenome: 'Martins',
-      idade: 14,
-      turno: 'Manhã',
-      materias: [
-        { name: 'Matemática', nota: 80 },
-        { name: 'Português', nota: 82 },
-        { name: 'Química', nota: 79 },
-        { name: 'Biologia', nota: 75 },
-      ],
-    },
-  ];
+// exercise 1
+const getPopulation = () => {
+  return countries
+  .reduce((acc ,curr) =>  acc + curr.population, 0) };
 
-const chetckBigger = (big, num) => (big.nota > num.nota) ? big : num;
+// exercise 2
+const getTotalArea = () => {
+  return countries
+  .reduce((acc, curr) => acc + curr.area, 0)};
 
+// exercise 3
+const longestName = () => {
+  return countries
+  .reduce((bigger, country) => bigger.name.length > country.name.length ? bigger : country)
+}
 
-  const etd = (std) => std.map((aluno) => {
-    return `name: ${aluno.nome}, melhorNota: ${aluno.materias.reduce(chetckBigger).name}`;
-  });
+// exercise 4
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
 
-  console.log(etd(estudantes));
+const countA = () => {
+  const formatNames = names.join('').toLocaleLowerCase().split('');
+  const count = formatNames.reduce((acc, curr) => curr.match('a') ? acc += 1 : acc, 0);
+  return count;
+};
+
+// exercise 5
+// O index 0 do array `students` equivale ao index 0 do array `grades`
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+const studentAverage = () => students
+  .map((std, index) => (
+    {
+    name: std,
+    average: grades[index]
+      .reduce((acc, curr) => acc + curr ,0) / grades[index].length,
+  }
+  ));
+
+console.log(studentAverage());
